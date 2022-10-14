9a99702d0d2a441f2cda3e2ea060a4fea293eb04
wryser <104174626+wryser@users.noreply.github.com>
2022-10-15 09:04:24 +1100

    Update 2.0

---
+++ SourceCode/ComputerInterface/GoProEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ComputerInterface;
+using ComputerInterface.Interfaces;
+
+namespace WrysersGoPro
+{
+    // An IComputerModEntry basically just describes an entry in the main menu that you can select
+    public class GoProEntry : IComputerModEntry
+    {
+        // This is the mod name that is going to show up as a selectable mod
+        public string EntryName => "Wryser's GoPro!";
+
+        // This is the first view that is going to be shown if the user select you mod
+        // The Computer Interface mod will instantiate your view 
+        public Type EntryViewType => typeof(GoProView);
+    }
+}
