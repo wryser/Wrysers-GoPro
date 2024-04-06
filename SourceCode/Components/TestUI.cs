b7c9007a0ae5755e17d28ba760d51691cd7044f9
wryser <104174626+wryser@users.noreply.github.com>
2024-04-06 20:25:34 +1100

    Add files via upload

---
+++ SourceCode/Components/TestUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WrysersGoPro
+{
+    public class TestUI : MonoBehaviour
+    {
+        public GameObject text;
+
+        public void TestTheUI()
+        {
+            text.SetActive(!text.active);
+        }
+
+        void Start()
+        {
+            text = GameObject.Find("UITesting(Clone)/Canvas/succeed");
+            text.SetActive(false);
+            GetComponent<Button>().onClick.AddListener(TestTheUI);
+        }
+    }
+}
