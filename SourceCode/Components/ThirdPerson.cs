543e07dc16a5631f2b261ae33d3d78af12b3442e
wryser <104174626+wryser@users.noreply.github.com>
2024-04-07 14:17:47 +1000

    Add files via upload

---
+++ SourceCode/Components/ThirdPerson.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WrysersGoPro.ConfigManager;
+using WrysersGoPro.Core;
+
+
+namespace WrysersGoPro
+{
+    public class ThirdPerson : MonoBehaviour
+    {
+        public GameObject text;
+        public bool isFaceCam = false;
+
+        public void FaceCamToggle()
+        {
+            GoProManager.thirdPerson = true;
+            GoProManager.topDown = false;
+            GoProManager.faceCam = false;
+        }
+
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(FaceCamToggle);
+        }
+    }
+}
---
+++ SourceCode/Components/TopDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WrysersGoPro.ConfigManager;
+using WrysersGoPro.Core;
+
+
+namespace WrysersGoPro
+{
+    public class TopDown : MonoBehaviour
+    {
+        public GameObject text;
+
+        public void TopDownToggle()
+        {
+            GoProManager.topDown = true;
+            GoProManager.faceCam = false;
+            GoProManager.thirdPerson = false;
+        }
+
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(TopDownToggle);
+        }
+    }
+}
